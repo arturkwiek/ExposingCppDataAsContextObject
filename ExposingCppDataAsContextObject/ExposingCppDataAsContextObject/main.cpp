ae18bee9f62b5be574d8e6fc46f84ab63763f33b
Artur Kwiek <artur.kwiek@zmt.tarnow.pl>
2020-04-16 14:30:51 +0200

    .

--- ExposingCppDataAsContextObject/ExposingCppDataAsContextObject/main.cpp
+++ ExposingCppDataAsContextObject/ExposingCppDataAsContextObject/main.cpp
@@ -1,4 +1,5 @@
 #include <QGuiApplication>
+#include <QQmlContext>
 #include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
@@ -7,7 +8,13 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
 
+    QString strFirstName = "John";
+    QString strLastName = "Doe";
+
     QQmlApplicationEngine engine;
+
+    engine.rootContext()->setContextProperty("mFirstName", QVariant::fromValue(strFirstName));
+    engine.rootContext()->setContextProperty("mLastName",QVariant::fromValue(strLastName));
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
